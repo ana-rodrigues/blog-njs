188ca7cd51c1e17cb62c50dc2433f3fa8d8b63c1
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-01-20 23:35:39 +0000

    Adds and styles about section

---
+++ app/components/about.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+
+        <h2 className='visually-hidden'>About</h2>
+
+        <div className='about'>
+
+            <div className='about-col col-2'>
+                <h3 className='heading-md'>About me</h3>
+                <p className='paragraph-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div className='about-col col-3'>
+                <h3 className='heading-md'>As a designer</h3>
+                <p className='paragraph-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+            <div className='about-col col-4'>
+                <h3 className='heading-md'>As a team leader</h3>
+                <p className='paragraph-sm'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+
+        </div>
+
+    </section>
+  )
+}
+
+
+
+export default About
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/hero';
 import ExperienceList from './components/experiencelist';
+import About from './components/about';
 
 export default function Page() {
   return (
     <>
       <Hero />
       <ExperienceList />
+      <About />
     </>
   );
 }
