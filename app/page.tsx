8ae4e76846f405bab3a128901583f674c1cf31b4
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 23:11:38 +0000

    Adds animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
 import Preheader from './components/preheader';
 import Hero from './components/hero';
 import Experience from './components/experience';
@@ -5,15 +9,48 @@ import About from './components/about';
 import Carousel from './components/carousel';
 import Footer from './components/footer';
 
-export default function Page () {
+const AnimatedSection = ({ children }) => {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 8 }}
+      animate={inView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.8, delay: 0.1 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default function Page() {
   return (
     <>
-      <Preheader />
-      <Hero />
-      <Carousel />
-      <Experience />
-      <About />
-      <Footer />
+      <AnimatedSection>
+        <Preheader />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <Hero />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <Carousel />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <Experience />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <About />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <Footer />
+      </AnimatedSection>
     </>
   );
 }
