1e609fd002c4df80c2078dd3e975d290dd09b634
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 21:38:44 +0000

    Remove unused nav reference

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Preheader from './components/preheader';
 import Hero from './components/hero';
 import Experience from './components/experience';
 import About from './components/about';
-import Nav from './components/nav';
 import Carousel from './components/carousel';
 
 export default function Page () {
