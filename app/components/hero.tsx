81a345b43712a6c31fc182699c7cee20f8f92f56
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 11:20:46 +0000

    Adjusts hero to mobile first version

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -19,8 +19,6 @@ return (
 
     <div className={styles.introBottom}>
 
-      <p></p>
-
       <p className={`paragraphMd ${styles.introDescription}`}>{`I'm a Product Experience Designer with over 8 years of experience in crafting digital solutions for the e-commerce and fintech industries. My experience spans roles as Product Designer, Team Lead, and Product Manager, putting me in a unique position to help businesses shape products zero to one.`}</p>
 
       <p className={`paragraphMd ${styles.introDescription}`}>{`I am a builder at heart, combining usability expertise, visual culture, strategic and technical skills. With hands-on familiarity with the complexities of B2B and API-first products, I thrive in collaborative, high-impact roles where thoughtful, well-crafted product experiences drive real business value.`}</p>
