3b697c8d18b485650c13b0fd36793cc2fdcc9b8f
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 19:30:43 +0000

    Refactor about section

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
+import styles from './about.module.css';
 
 const About = () => {
   return (
-    <section>
+    <section className='container'>
 
-        <h2 className='visuallyHidden'>About</h2>
+        <h2 className='sectionHeader monoMd'>About me</h2>
 
-        <div className='about container'>
+        <div className={`${styles.about}`}>
 
-            <div className='aboutCol'>
-                <h3 className='headingMd'>About me</h3>
+            <div className={`${styles.aboutCol}`}>
                 <p className='paragraphMd'>{'My first contact with digital experiences was an old Olivetti machine, where I quickly learned to run my favourite games as a kid, through obsessive trial and error. It was the entry point to becoming passionately interested about technology, culture and design.'}</p>
             </div>
 
-            <div className='aboutCol'>
-                <h3 className='headingMd'>As a designer</h3>
+            <div className={`${styles.aboutCol}`}>
                 <p className='paragraphMd'>{'I have guided multiple early-stage companies in shaping their product experiences to find market-fit in dynamic, highly competitive markets. This required exceptional craft, rapid execution, strong collaboration and navigating ambiguity to deliver impactful results.'}</p>
-            </div>
-
-            <div className='aboutCol'>
-                <h3 className='headingMd'>As a team leader</h3>
-                <p className='paragraphMd'>{'I had the opportunity to build and lead high-performing teams, fostering design cultures that transformed companies from emerging players to innovators. These teams nurtured young designers, who have since built successful careers of their own.'}</p>
             </div>
 
         </div>
