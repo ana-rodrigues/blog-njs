9cc20cf60475269384c9876d9e86985f031d6c0d
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 13:28:47 +0000

    Adds scrollable carousel to hero (non-styled)

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,30 +1,31 @@
+import React from 'react';
+import ImageCarousel from './carousel';
 import styles from './hero.module.css';
 
 const Hero: React.FC = () => {
-  const thumbnails = [
+  const images = [
+    '/media/swell-thumb.png',
+    '/media/swell-thumb.png',
     '/media/swell-thumb.png',
     '/media/swell-thumb.png',
     '/media/swell-thumb.png',
     '/media/swell-thumb.png',
   ];
 
-  
-return (
+  return (
     <section className={`container ${styles.hero}`}>
+      <div className={styles.introTop}>
+        <p className={styles.introHeading}>{'Full-stack product experience designer for fintech, e-commerce, and everything in between.'}</p>
+      </div>
 
-    <div className={styles.introTop}>
-      <p className={styles.introHeading}>{'Full-stack product experience designer for fintech, e-commerce, and everything in between.'}</p>
-    </div>
+      <div className={styles.introBottom}>
+        <p className={`paragraphMd ${styles.introDescription}`}>{`I'm a Product Experience Designer with over 8 years of experience in crafting digital solutions for the e-commerce and fintech industries. My experience spans roles as Product Designer, Team Lead, and Product Manager, putting me in a unique position to help businesses shape products zero to one.`}</p>
+        <p className={`paragraphMd ${styles.introDescription}`}>{`I am a builder at heart, combining usability expertise, visual culture, strategic and technical skills. With hands-on familiarity with the complexities of B2B and API-first products, I thrive in collaborative, high-impact roles where thoughtful, well-crafted product experiences drive real business value.`}</p>
+      </div>
 
-    <div className={styles.introBottom}>
-
-      <p className={`paragraphMd ${styles.introDescription}`}>{`I'm a Product Experience Designer with over 8 years of experience in crafting digital solutions for the e-commerce and fintech industries. My experience spans roles as Product Designer, Team Lead, and Product Manager, putting me in a unique position to help businesses shape products zero to one.`}</p>
-
-      <p className={`paragraphMd ${styles.introDescription}`}>{`I am a builder at heart, combining usability expertise, visual culture, strategic and technical skills. With hands-on familiarity with the complexities of B2B and API-first products, I thrive in collaborative, high-impact roles where thoughtful, well-crafted product experiences drive real business value.`}</p>
-    </div>
-
+      <ImageCarousel images={images} />
     </section>
-)
-}
+  );
+};
 
-export default Hero
+export default Hero;
