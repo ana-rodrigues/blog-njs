a3070e72639c47d6d22c1e4c4901f305ab0071b0
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-01 23:21:56 +0000

    Remove delay

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const AnimatedSection = ({ children }) => {
       ref={ref}
       initial={{ opacity: 0, y: 8 }}
       animate={inView ? { opacity: 1, y: 0 } : {}}
-      transition={{ duration: 0.8, delay: 0.1 }}
+      transition={{ duration: 0.8 }}
     >
       {children}
     </motion.div>
