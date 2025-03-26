ed2c0f18a3f9b1e3ab4cc40d3e66ed198e88eeb6
ana-rodrigues <a.rodrigues.email@gmail.com>
2025-03-26 23:57:33 +0000

    Adjusts showcase

--- app/components/showcase.tsx
+++ app/components/showcase.tsx
@@ -78,7 +78,7 @@ export function Showcase() {
                   className={styles.showcaseImage}
                   draggable="false"
                   unoptimized={false}
-                />
+                /> 
               </div>
             </div>
           ))}
